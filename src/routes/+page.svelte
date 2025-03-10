617b10c57005158e6c81e223c38b1fc09f94c068
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-03-10 02:19:57 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Tooltip } from 'bits-ui';
+
+	let asteriskRef = $state<HTMLElement | null>(null);
+</script>
+
+<Tooltip.Provider>
+	<Tooltip.Root>
+		<Tooltip.Trigger>Hello <span bind:this={asteriskRef}>*</span></Tooltip.Trigger>
+		<Tooltip.Portal>
+			<Tooltip.Content customAnchor={asteriskRef}>
+				World
+				<Tooltip.Arrow />
+			</Tooltip.Content>
+		</Tooltip.Portal>
+	</Tooltip.Root>
+</Tooltip.Provider>
